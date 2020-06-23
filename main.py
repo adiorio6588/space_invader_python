ac9dd831089217aa9cef31619fe2469381c798e2
adiorio6588 <diorio.anthony11b@gmail.com>
2020-06-23 15:20:39 -0700

    Added math for the game collisions.  Added multiple enemies to the screen

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import math
 import random
 
 # Intialize the pygame
@@ -6,6 +7,9 @@ pygame.init()
 
 # Create the screen
 screen = pygame.display.set_mode((800, 600))
+
+# Background
+background = pygame.image.load('background.png')
 
 # Title and Icon
 pygame.display.set_caption("Space Invaders")
@@ -20,25 +24,61 @@ playerX_change = 0
 
 
 # Enemy
-enemyImg = pygame.image.load('enemy.png')
-enemyX = random.randint(0, 800)
-enemyY = random.randint(50, 150)
-enemyX_change = 0
+enemyImg  = []
+enemyX = []
+enemyY = []
+enemyX_change = []
+enemyY_change = []
+num_of_enemies = 6
 
-def player(x,y):
+for i in range(num_of_enemies):
+    enemyImg.append(pygame.image.load('enemy.png'))
+    enemyX.append(random.randint(0, 736))
+    enemyY.append(random.randint(50, 150))
+    enemyX_change.append(4)
+    enemyY_change.append(40)
+
+# Missile
+# Ready - You can't see the missle on the screen
+# Fire - The missile is currently moving
+missileImg = pygame.image.load('missile.png')
+missileX = 0
+missileY = 480
+missileX_change = 0
+missileY_change = 10
+missile_state = "ready"
+
+# Score
+
+def player(x, y):
     screen.blit(playerImg, (x, y))
 
 
-def enemy(x,y):
-    screen.blit(enemyImg, (x, y))
+def enemy(x, y, i):
+    screen.blit(enemyImg[i], (x, y))
+
+def fire_missile(x, y):
+    global missile_state
+    missile_state = "fire"
+    screen.blit(missileImg, (x + 16, y + 10)) # For the missle to appear in front
+
+
+def isCollision(enemyX, enemyY, missileX, missileY):
+    distance = math.sqrt(math.pow(enemyX - missileX, 2) + (math.pow(enemyY - missileY, 2)))
+    if distance < 27:
+        return True
+    else:
+        return False 
+
 
 # Game Loop
 running = True
 while running:
 
-    #RGB - R    Red, Green, Blue
+    # RGB - R    Red, Green, Blue
     screen.fill((0, 0, 0))
-
+    # Background Image
+    screen.blit(background, (0, 0))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
@@ -46,9 +86,15 @@ while running:
         # if keystroke is pressed check whether its right or left
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                playerX_change = -0.3
+                playerX_change = -7
             if event.key == pygame.K_RIGHT:
-                playerX_change = 0.3
+                playerX_change = 7
+            if event.key == pygame.K_SPACE:
+                if missile_state is "ready":
+                    # Get the x coordinate of the spaceship
+                    missileX = playerX
+                    fire_missile(playerX, missileY)
+
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                 playerX_change = 0 
@@ -56,6 +102,7 @@ while running:
     # 5 = 5 + -0.1 -> 5 = 5 - 0.1
     # 5 = 5 + 0.1
 
+    # Checking for boundaries of spaceship so it doesn't go out of bounds
     playerX += playerX_change
 
     if playerX <= 0:
@@ -63,6 +110,36 @@ while running:
     elif playerX >= 736:
         playerX = 736
 
+    # Enemy Movement
+    for i in range(num_of_enemies):
+        enemyX[i] += enemyX_change[i]
+        if enemyX[i] <= 0:
+            enemyX_change[i] = 4
+            enemyY[i] += enemyY_change[i]
+        elif enemyX[i] >= 736:
+            enemyX_change[i] = -4
+            enemyY[i] += enemyY_change[i]
+
+        # Collision
+        collision = isCollision(enemyX[i], enemyY[i], missileX, missileY)
+        if collision:
+            missileY = 480
+            missile_state = "ready"
+            score_value += 1
+            enemyX[i] = random.randint(0, 736)
+            enemyY[i] = random.randint(50, 150)
+
+        enemy(enemyX[i], enemyY[i], i)
+
+    # Missile Movement 
+    if missileY <= 0:
+        missileY = 480
+        missile_state = "ready"
+
+    if missile_state is "fire":
+        fire_missile(missileX, missileY)
+        missileY -= missileY_change
+    
+
     player(playerX, playerY)
-    enemy(enemyX, enemyY)
     pygame.display.update()
